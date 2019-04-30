680f5eddb904ac8c585d5e20228b7b6628527abf
JLMarcos <jlmarcos1998@hotmail.com>
2019-04-30 12:35:31 -0500

    Cambios

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,7 +22,8 @@ window.Vue = require('vue');
 
 Vue.component('sale-component', require('./components/SaleComponent.vue').default);
 
-Vue.prototype.$appUrl = 'http://mastergis.test/';
+// Vue.prototype.$appUrl = 'http://mastergis.test/';
+Vue.prototype.$appUrl = 'https://mastergis.herokuapp.com/';
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
