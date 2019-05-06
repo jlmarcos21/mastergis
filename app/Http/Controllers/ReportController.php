6f7f7f49ce1880d3efba8628e9c77f417f56dae9
JLMarcos <jlmarcos1998@hotmail.com>
2019-05-06 18:05:17 -0500

    Change ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -11,27 +11,29 @@ class ReportController extends Controller
 {
 
     public function search_sales(Request $request)
-    {
-        
+    {           
         if(isset($request->date_s) && isset($request->date_f)) {
             $sales = Sale::orderBy('id', 'DESC')
                         ->whereBetween('date', [$request->date_s, $request->date_f])
                         ->get();
-        }
-        
-        return view('reports.sales_consultation', compact('sales', 'request'));
+
+            return view('reports.sales_consultation', compact('sales', 'request'));
+        }else {
+            return view('reports.sales_consultation', compact('request'));
+        }            
     }
 
     public function search_courses(Request $request)
-    {
-        
+    {        
         if(isset($request->date_s) && isset($request->date_f)) {
             $s_courses = DetailSale::orderBy('date', 'DESC')
                         ->whereBetween('date', [$request->date_s, $request->date_f])                                     
-                        ->get();                            
-        }
-        
-        return view('reports.courses_consultation', compact('s_courses', 'request'));
+                        ->get();
+
+            return view('reports.courses_consultation', compact('request'));                 
+        }else {
+            return view('reports.courses_consultation', compact('s_courses', 'request'));
+        }                
     }
 
 }
